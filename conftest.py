649dbaeb15ddf494aa1e16c7ef7337f7b9fac55f
Ruslan <47752953+ijevius@users.noreply.github.com>
2023-06-10 15:48:12 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -34,7 +34,7 @@ def config(request):
 @pytest.fixture(scope='function')
 def driver(config):
     browser = webdriver.Remote(
-        command_executor="http://192.168.1.39:4444/",
+        command_executor="http://192.168.1.39:4444/wd/hub",
         desired_capabilities=capabilities)
 
     browser.maximize_window()
