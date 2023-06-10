285c50663fd8ef1a49568e38f0be8dfc31acc098
Ruslan <47752953+ijevius@users.noreply.github.com>
2023-06-10 18:16:52 +0300

    Update test_simple.py

--- ui/test_ui/test_simple.py
+++ ui/test_ui/test_simple.py
@@ -7,9 +7,11 @@ from ui.test_ui.base import BaseCase
 @pytest.mark.UI
 class Test1(BaseCase):
     @pytest.mark.UI
+    @allure.feature('Ввод текста')
     def test_login(self):
         self.page.enter_text()
-        assert self.page.find(locators.MainPageLocators.P_BLOCK_LOCATOR).text == "ЦГАТ02ГП230ПЕ"
+        with allure.step("Сравниваем что ввели и что показывает"):
+            assert self.page.find(locators.MainPageLocators.P_BLOCK_LOCATOR).text == "ЦГАТ02ГП230ПЕ"
         #time.sleep(1500)
         #assert True
 
